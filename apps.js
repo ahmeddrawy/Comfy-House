79a56729c3e9eed35c5d80ceccd2ae812eef16a5
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2020-07-21 18:21:53 +0200

    main structure for the app.ks and fetching products

--- apps.js
+++ apps.js
@@ -9,4 +9,32 @@ const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 const productsDOM = document.querySelector('.products-center');
 let cart = []
+// classes 
+// class for products and get them
+class Products{
+    async getProducts(){
+        try {
+            let result = await fetch('products.json');
+            let data=  await result.json();
+            return data;
+        } catch (error) {
+            console.log('error -1');
+            console.log(error);
+        }
+    }
+}
+// display products
+class UI{
 
+}
+/// local storage class
+class Storage{
+
+}
+/// listener
+document.addEventListener("DOMContentLoaded" , function(){
+    const ui = new UI();
+    const products = new Products();
+    products.getProducts().then(result=>console.log(result));
+
+})
