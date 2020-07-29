a4b459050b5ac5e55a49c9b3fbab4075054e9859
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2020-07-29 17:33:06 +0200

    add event listener to remove items from cart using remove btn

--- apps.js
+++ apps.js
@@ -138,7 +138,16 @@ class UI{
     cartLogic(){
         /// clear cart btn
         clearCartBtn.addEventListener('click',()=>{this.clearCart()});
-        
+        ///cart functuinality
+        cartContent.addEventListener('click',event=>{
+            console.log(event.target);
+            if(event.target.classList.contains('remove-item')){
+                let removeItem = event.target;
+                let id = removeItem.getAttribute('data-id');
+                cartContent.removeChild(removeItem.parentElement.parentElement);
+                this.removeItem(id);
+            }
+        })
     }
     clearCart(){
         let cartItems = cart.map(item=>item.id);
